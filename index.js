f8c344a50624f41af1a632317c8bb43d25d08842
Ken Sin <ken.sin@ticketfly.com>
2017-01-26 15:08:43 -0800

    Import styles with addon

--- index.js
+++ index.js
@@ -2,5 +2,13 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-ticketfly-buttons'
+  name: 'ember-ticketfly-buttons',
+
+  included: function(app) {
+
+    app.import('app/styles/ember-ticketfly-buttons.css');
+
+    this._super.included.call(this, app);
+
+  }
 };
